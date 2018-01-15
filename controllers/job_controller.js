f70b48d37ecc492db7860cf4a78ab4ccf14323d6
manishavi <manishalal972@gmail.com>
2018-01-15 17:37:13 -0600

    cleaned

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -2,23 +2,19 @@ const jobs = require('../config/jobs');
 
 module.exports = {
   create(req, res) {
-    //get the id, title, duration, description from request body
-  let id = req.body.id;
-  let title = req.body.title;
-  let duration = req.body.duration;
-  let description = req.body.description;
-  //create new job object
-  let job = {
-    id,
-    title,
-    description,
-    duration
-  };
-  //add object to jobs array
-  jobs.push(job);
-  //return jobs array to server
-  if (!req.body) return res.sendStatus(400);
-  return res.json(jobs);
+    let title = req.body.title;
+    let duration = req.body.duration;
+    let description = req.body.description;
+
+    let job = {
+      title,
+      description,
+      duration
+    };
+
+    jobs.push(job);
+    if (!req.body) return res.sendStatus(400);
+    return res.json(jobs);
   },
   findAll(req, res) {
     return res.json(jobs);
