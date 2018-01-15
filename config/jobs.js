5c8559766a34b7e3d47f0b9abeb2f9c7e435fc7a
manishavi <manishalal972@gmail.com>
2018-01-15 12:51:37 -0600

    restructuring

---
+++ config/jobs.js
@@ -0,0 +1,24 @@
+//create jobs object
+//each object will have id, title,description and duration
+const jobs = [
+  {
+    id: 1,
+    title: "Node",
+    description: "For backend",
+    duration: "3 months"
+  },
+  {
+    id: 2,
+    title: "React",
+    description: "For front-end",
+    duration: "3 months"
+  },
+  {
+    id: 3,
+    title: "Node-express",
+    description: "For server",
+    duration: "3 months"
+  }
+];
+
+module.exports = jobs;
