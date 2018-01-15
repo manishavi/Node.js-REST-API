1c586ca03f5e2bdff333d18df4212f1efde7975a
manishavi <manishalal972@gmail.com>
2018-01-15 11:13:12 -0600

    created jobs endpoint

--- app.js
+++ app.js
@@ -8,8 +8,36 @@ const app = express();
 app.get("/", function(req, res) {
   //set the root route
   res.send("hello world");
-  //send the message
 });
+//create jobs object
+//each object will have id, title,description and duration
+const jobs = [
+  {
+    id: 1,
+    title: "Node",
+      description: "For backend",
+      duration: "3 months"
+    }, 
+    {
+      id: 2,
+      title: "React",
+      description: "For front-end",
+      duration: "3 months"
+    }, 
+    {
+      id: 3,
+      title: 'Node-express',
+      description: 'For server',
+      duration: '3 months'
+    }
+]
+//create jobs endpoint
+app.get("/jobs", function (req, res) {
+  res.json(jobs);
+});
+//return jobs to server
+
+
 //listen the express app on port 3000
 app.listen(3000, () => {
   console.log('listening on port 3000');
