9cfbf3099002237450fd88e346cd7529757cb8c0
manishavi <manishalal972@gmail.com>
2018-01-15 14:40:19 -0600

    connected app to mongoose

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 "use strict";
-//import express
 const express = require("express");
 const bodyParser = require("body-parser");
-//create express app
 const app = express();
-
 const routes = require('./routes/index');
+//import mongoose module
+const mongoose = require('mongoose');
+const config = require('./config/config.json');
 
-//add the middleware
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({
   extended: true
-})); // for parsing application/x-www-form-urlencoded
-
-//add custom middleware
-app.use((req, res, next) => {
-  //log the message, Custom middleware run
-  req.userId = 1;
-  req.user = 'Manisha Lal';
-  console.log("Custom middleware run");
-  //call the next middleware
-  next();
-});
+})); 
 
 app.get("/", (req, res) => {
-  //set the root route
   console.log(req.userId);
   console.log(req.user);
   res.send("hello world");
 });
 
 app.use('/api', routes); //allthe backend routes will start from (/api)
+
+//connect the app to mongoose
+const promise = mongoose.connect(config.MONGO_URI, () => {
+  console.log('App is connected to mongoDB');
+  useMongoClient: true
+});
+//App is connected to mongoDB
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
