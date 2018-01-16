f3cb407379252877d6c8ce8d0f36bd039556051f
manishavi <manishalal972@gmail.com>
2018-01-15 19:46:38 -0600

    added new job, find all jobs

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -1,22 +1,48 @@
 const jobs = require('../config/jobs');
+const Job = require('../models/job_model');
 
 module.exports = {
   create(req, res) {
     let title = req.body.title;
     let duration = req.body.duration;
     let description = req.body.description;
-
+    //make sure title is provided by user
+    if(!title) {
+      return res.json({status: 400, error:"title is required"});
+    }
+    //if title missing send error status 400
     let job = {
       title,
       description,
       duration
     };
 
-    jobs.push(job);
-    if (!req.body) return res.sendStatus(400);
-    return res.json(jobs);
+    // jobs.push(job);
+    // if (!req.body) return res.sendStatus(400);
+    // return res.json(jobs);
+    //create new instance of jobmodel
+    //pass job object in constructor function
+    const newJob = new Job(job);
+    //save the job
+    //when job saved successfully then send the new job
+    //to the server with status code 200
+    newJob.save(err => {
+      if (err) {
+        return res.json({ status: 500, error: err });
+      }
+      console.log('saved');
+      re.json({ status: 500, newJob });
+    })
   },
   findAll(req, res) {
-    return res.json(jobs);
+    //call find() of JobModel
+    Job.find({}, (err, jobs) => {
+      if(err) {
+        return res.json({ status: 404, error: err });
+      }
+      res.json({ status: 200, job });
+    })
+    //if err send err with 404
+    //return all jobs to server with 200
   }
 };
