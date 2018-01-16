f007069bc4f5288d163c23f5bef9d1cc43220cc3
manishavi <manishalal972@gmail.com>
2018-01-15 19:44:14 -0600

    cleaned

--- models/job_model.js
+++ models/job_model.js
@@ -2,24 +2,16 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-//create new JobSchema
 const JobSchema = new Schema({
-  //set title property to required
   title: {
     type: String,
     required: true
   },
-  //add description property
-  //set description to string
   description: {
     type: String
   },
-  //add duration property
-  //set the typeof duration to string
   duration: {
     type: String,
   }
 });
-
-//export the instance of model by passing JobSchema
 module.exports = mongoose.model('Job', JobSchema);
